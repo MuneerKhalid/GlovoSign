c9eff0fd52985f10809a1293460d3792a1e0ce14
MuneerKhalid <muneer.ansari.2646@gmail.com>
2024-01-06 11:21:53 +0500

    googlbtn added

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,24 +1,23 @@
-'use client'
+"use client";
 
-
-import Link from 'next/link'
-import { Router } from 'next/router';
-import {useState} from 'react'
+import Link from "next/link";
+import { Router } from "next/router";
+import { useState } from "react";
 
 export default function SignUp() {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [status, setStatus] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [status, setStatus] = useState("");
 
   const handleSignup = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await fetch('api/signup', {
-        method: 'POST',
+      const response = await fetch("api/signup", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ username, email, password, status }),
       });
@@ -27,21 +26,21 @@ export default function SignUp() {
 
       if (data.success) {
         // Signup successful, you can redirect or show a success message
-        console.log('Signup successful:', data.message);
-        setUsername('');
-        setEmail('');
-        setPassword('');
-        setStatus('');
+        console.log("Signup successful:", data.message);
+        setUsername("");
+        setEmail("");
+        setPassword("");
+        setStatus("");
       } else {
         // Signup failed, handle errors
-        console.error('Signup failed:', data.message);
-        setUsername('');
-        setEmail('');
-        setPassword('');
-        setStatus('');
+        console.error("Signup failed:", data.message);
+        setUsername("");
+        setEmail("");
+        setPassword("");
+        setStatus("");
       }
     } catch (error) {
-      console.error('Error during signup:', error.message);
+      console.error("Error during signup:", error.message);
     }
   };
 
@@ -49,7 +48,6 @@ export default function SignUp() {
     <section className="bg-gradient-to-b from-gray-100 to-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
           {/* Page header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h1 className="h1">Welcome. We exist to make Life easier.</h1>
@@ -60,75 +58,141 @@ export default function SignUp() {
             <form onSubmit={handleSignup}>
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Username <span className="text-red-600">*</span></label>
-                  <input 
-                    id="username" type="text" 
-                    value={username} 
-                    onChange={(e) => { setUsername(e.target.value)}}
-                    className="form-input w-full text-gray-800" 
-                    placeholder="User Name..." 
-                    required 
-                  />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email <span className="text-red-600">*</span></label>
-                  <input 
-                    id="email" type="email"
-                    value={email} 
-                    onChange={(e) => { setEmail(e.target.value)}}
-                    className="form-input w-full text-gray-800" 
-                    placeholder="Email Address.." 
+                  <label
+                    className="block text-gray-800 text-sm font-medium mb-1"
+                    htmlFor="name"
+                  >
+                    Username <span className="text-red-600">*</span>
+                  </label>
+                  <input
+                    id="username"
+                    type="text"
+                    value={username}
+                    onChange={(e) => {
+                      setUsername(e.target.value);
+                    }}
+                    className="form-input w-full text-gray-800"
+                    placeholder="User Name..."
                     required
                   />
                 </div>
               </div>
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="password">Password <span className="text-red-600">*</span></label>
-                  <input 
-                    id="password" type="password" 
-                    value={password} 
-                    onChange={(e) => { setPassword(e.target.value)}}
-                    className="form-input w-full text-gray-800" 
-                    placeholder="Your Password..." 
-                    required 
+                  <label
+                    className="block text-gray-800 text-sm font-medium mb-1"
+                    htmlFor="email"
+                  >
+                    Email <span className="text-red-600">*</span>
+                  </label>
+                  <input
+                    id="email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                    className="form-input w-full text-gray-800"
+                    placeholder="Email Address.."
+                    required
                   />
                 </div>
               </div>
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="status">Status <span className="text-red-600">*</span></label>
-                  <input 
-                    id="status" type="text" 
-                    value={status} 
-                    onChange={(e) => { setStatus(e.target.value)}}
-                    className="form-input w-full text-gray-800" 
-                    placeholder="Normal/Disabled?..." 
-                    required 
+                  <label
+                    className="block text-gray-800 text-sm font-medium mb-1"
+                    htmlFor="password"
+                  >
+                    Password <span className="text-red-600">*</span>
+                  </label>
+                  <input
+                    id="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    className="form-input w-full text-gray-800"
+                    placeholder="Your Password..."
+                    required
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-4">
+                <div className="w-full px-3">
+                  <label
+                    className="block text-gray-800 text-sm font-medium mb-1"
+                    htmlFor="status"
+                  >
+                    Status <span className="text-red-600">*</span>
+                  </label>
+                  <input
+                    id="status"
+                    type="text"
+                    value={status}
+                    onChange={(e) => {
+                      setStatus(e.target.value);
+                    }}
+                    className="form-input w-full text-gray-800"
+                    placeholder="Normal/Disabled?..."
+                    required
                   />
                 </div>
               </div>
               <div className="flex flex-wrap -mx-3 mt-6">
                 <div className="w-full px-3">
-                  <button type='submit' className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">Sign up</button>
+                  <button
+                    type="submit"
+                    className="btn text-white bg-blue-600 hover:bg-blue-700 w-full"
+                  >
+                    Sign up
+                  </button>
                 </div>
               </div>
             </form>
             <div className="flex items-center my-6">
-              <div className="border-t border-gray-300 grow mr-3" aria-hidden="true"></div>
+              <div
+                className="border-t border-gray-300 grow mr-3"
+                aria-hidden="true"
+              ></div>
               <div className="text-gray-600 italic">Or</div>
-              <div className="border-t border-gray-300 grow ml-3" aria-hidden="true"></div>
+              <div
+                className="border-t border-gray-300 grow ml-3"
+                aria-hidden="true"
+              ></div>
             </div>
-            
+            <form>
+              <div className="flex flex-wrap -mx-3">
+                <div className="w-full px-3">
+                  <button className="btn px-0 text-white bg-red-600 hover:bg-red-700 w-full relative flex items-center">
+                    <svg
+                      className="w-4 h-4 fill-current text-white opacity-75 shrink-0 mx-4"
+                      viewBox="0 0 16 16"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M7.9 7v2.4H12c-.2 1-1.2 3-4 3-2.4 0-4.3-2-4.3-4.4 0-2.4 2-4.4 4.3-4.4 1.4 0 2.3.6 2.8 1.1l1.9-1.8C11.5 1.7 9.9 1 8 1 4.1 1 1 4.1 1 8s3.1 7 7 7c4 0 6.7-2.8 6.7-6.8 0-.5 0-.8-.1-1.2H7.9z" />
+                    </svg>
+                    <span className="flex-auto pl-16 pr-8 -ml-16">
+                      Continue with Google
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </form>
+
             <div className="text-gray-600 text-center mt-6">
-              Already Using GlovoSign? <Link href="/signin" className="text-blue-600 hover:underline transition duration-150 ease-in-out">Sign in</Link>
+              Already Using GlovoSign?{" "}
+              <Link
+                href="/signin"
+                className="text-blue-600 hover:underline transition duration-150 ease-in-out"
+              >
+                Sign in
+              </Link>
             </div>
           </div>
-
         </div>
       </div>
     </section>
-  )
+  );
 }
