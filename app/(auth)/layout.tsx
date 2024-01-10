dd0cf1d81f524c25a6b61bba45436b9646b5034a
MuneerKhalid <muneer.ansari.2646@gmail.com>
2024-01-10 20:50:26 +0500

    layout changes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,13 @@
 export default function AuthLayout({
   children,
 }: {
-  children: React.ReactNode
-}) {  
+  children: React.ReactNode;
+}) {
   return (
     <main className="grow">
-
-      {children}
-
+      <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
+        {children}
+      </div>
     </main>
-  )
+  );
 }
