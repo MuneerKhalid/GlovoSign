a15b1489efc73b183e0453a181b6eeeeeef89130
MuneerKhalid <muneer.ansari.2646@gmail.com>
2024-03-14 15:27:55 +0500

    mess

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -1,25 +1,28 @@
-'use client'
-import { pusherClient } from '@/lib/pusher'
-import { FC, useEffect, useState } from 'react'
+"use client";
+import { pusherClient } from "@/lib/pusher";
+import { FC, useEffect, useState } from "react";
 
 interface MessagesProps {
-  roomId: string
+  roomId: string;
 }
 
 const Messages: FC<MessagesProps> = ({ roomId }) => {
-  const [incomingMessages, setIncomingMessages] = useState<string[]>([])
+  const [incomingMessages, setIncomingMessages] = useState<string[]>([]);
 
   useEffect(() => {
-    pusherClient.subscribe(roomId)
+    pusherClient.subscribe(roomId);
 
-    pusherClient.bind('incoming-message', (text: string) => {
-      setIncomingMessages((prev) => [...prev, text])
-    })
+    const handleIncomingMessage = (text: string) => {
+      setIncomingMessages((prev) => [...prev, text]);
+    };
+
+    pusherClient.bind("incoming-message", handleIncomingMessage);
 
     return () => {
-      pusherClient.unsubscribe(roomId)
-    }
-  }, [])
+      pusherClient.unsubscribe(roomId);
+      pusherClient.unbind("incoming-message", handleIncomingMessage);
+    };
+  }, [roomId]);
 
   return (
     <div>
@@ -30,7 +33,7 @@ const Messages: FC<MessagesProps> = ({ roomId }) => {
         <p key={i}>{text}</p>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Messages
+export default Messages;
