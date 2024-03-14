ba4cd480c0c5e20374eed7c3b12aa50dcf1b1078
MuneerKhalid <muneer.ansari.2646@gmail.com>
2024-03-14 15:14:35 +0500

    new

--- components/MessageField.tsx
+++ components/MessageField.tsx
@@ -8,27 +8,37 @@ interface MessageFieldProps {
 }
 
 const MessageField: FC<MessageFieldProps> = ({ roomId }) => {
-  // let input = ''
   const [input, setInput] = useState<string>('');
 
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
 
   const sendMessage = async (text: string) => {
-    await axios.post('/api/message', { text, roomId })
+    await axios.post('/api/message', { text, roomId });
+    // Clear the input field after the message is sent
     setInput('');
-  }
+  };
+
+  const handleSendClick = () => {
+    // Ensure that input has a value before sending the message
+    if (input.trim() !== '') {
+      sendMessage(input);
+    }
+  };
 
   return (
     <div className='flex gap-2'>
-      type a new message:
+      Type a new message:
       <input
-        onChange={({ target }) => setInput(target.value)}
+        onChange={handleInputChange}
         className='border border-zinc-300'
         type='text'
         value={input}
       />
-      <button onClick={() => sendMessage(input || '')}>send</button>
+      <button onClick={handleSendClick}>Send</button>
     </div>
-  )
-}
+  );
+};
 
-export default MessageField
+export default MessageField;
