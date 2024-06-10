b1a55926da1aa270c3cd07a4864be636cdf913e9
MuneerKhalid <muneer.ansari.2646@gmail.com>
2024-06-10 21:39:53 +0500

    new

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <UserButton />
+    
+  )
+}
+
+export default Home
---
+++ providers/ConvexClientProvider.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import React from 'react';
+import { ClerkProvider, useAuth, SignIn } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import { ConvexReactClient, Authenticated, AuthLoading, Unauthenticated } from 'convex/react';
+import LoadingLogo from '@/components/shared/LoadingLogo';
+
+type Props = {
+  children: React.ReactNode
+};
+
+const CONVEX_URL = process.env.NEXT_PUBLIC_CONVEX_URL || ""
+
+const convex = new ConvexReactClient(CONVEX_URL);
+
+const ConvexClientProvider = ({ children }: Props) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <Unauthenticated>
+          <SignIn />
+        </Unauthenticated>
+        <Authenticated>{children}</Authenticated>
+        <AuthLoading>
+          <LoadingLogo />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  )
+}
+
+export default ConvexClientProvider
