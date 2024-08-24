a42c29c2ae50d1c8483758d7400b49b2904693ea
Muneer Khalid <99136405+MuneerKhalid@users.noreply.github.com>
2024-08-24 11:28:16 +0500

    Update ConvexClientProvider.tsx

--- providers/ConvexClientProvider.tsx
+++ providers/ConvexClientProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect } from "react";
-import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ClerkProvider, useAuth, SignIn } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import {
   ConvexReactClient,
@@ -19,20 +19,20 @@ const CONVEX_URL = process.env.NEXT_PUBLIC_CONVEX_URL || "";
 
 const convex = new ConvexReactClient(CONVEX_URL);
 
-const RedirectToSignIn = () => {
-  useEffect(() => {
-    window.location.href = "https://rested-serval-15.accounts.dev/sign-in";
-  }, []);
+// const RedirectToSignIn = () => {
+//   useEffect(() => {
+//     window.location.href = "https://rested-serval-15.accounts.dev/sign-in";
+//   }, []);
 
-  return null; 
-};
+//   return null; 
+// };
 
 const ConvexClientProvider = ({ children }: Props) => {
   return (
     <ClerkProvider>
       <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
         <Unauthenticated>
-          <RedirectToSignIn />
+          <SignIn />
         </Unauthenticated>
         <Authenticated>{children}</Authenticated>
         <AuthLoading>
